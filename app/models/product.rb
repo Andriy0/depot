b112ef3cc5929231356c99c8facce9e2ca33845e
Andriy0 <andriy2152@gmail.com>
2023-06-23 21:15:25 +0300

    Tweak product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, :image_url, presence: { message: 'cannot be blank.' }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with:    /\.(gif|jpg|png)\Z/i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  validates :description, allow_blank: true, length: { minimum: 10 }
 end
