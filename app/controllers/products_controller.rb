8e20ef9b8afb45e92fcbab6c2d502b6513ed488c
Andriy0 <andriy2152@gmail.com>
2023-06-28 11:48:48 +0300

    Check Out!

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -62,6 +62,15 @@ class ProductsController < ApplicationController
     end
   end
 
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+
+    return unless stale?(@latest_order)
+
+    respond_to(&:atom)
+  end
+
   private
 
   def invalid_product
